bbeb45e01c772e315624f550f6084dd179e79df4
Kenji Abe <kenji.01.star@gmail.com>
2020-12-12 19:12:54 +0900

    Migrate application final

--- app/src/main/java/com/star_zero/migrate_hilt/App.kt
+++ app/src/main/java/com/star_zero/migrate_hilt/App.kt
@@ -1,22 +1,11 @@
 package com.star_zero.migrate_hilt
 
+import android.app.Application
 import com.star_zero.migrate_hilt.core.util.AppConfig
-import com.star_zero.migrate_hilt.di.AppComponent
-import dagger.android.AndroidInjector
-import dagger.android.support.DaggerApplication
-import dagger.hilt.EntryPoints
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : DaggerApplication() {
-
-    private val appComponent: AppComponent by lazy {
-        EntryPoints.get(this, AppComponent::class.java)
-    }
-
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return appComponent
-    }
+class App : Application() {
 
     fun getAppConfig(): AppConfig {
         return AppConfig(BuildConfig.DEBUG)
